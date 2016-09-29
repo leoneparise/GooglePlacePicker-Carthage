85714210982de59e29faec097c01a64cd88b0546
Leone Parise <leone.parise@gmail.com>
2016-09-29 15:08:18 -0300

    Initial Commit

---
+++ GooglePlacePicker/GooglePlacePicker.h
@@ -0,0 +1,19 @@
+//
+//  GooglePlacePicker.h
+//  GooglePlacePicker
+//
+//  Created by Leone Parise Vieira da Silva on 29/09/16.
+//  Copyright © 2016 com.google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GooglePlacePicker.
+FOUNDATION_EXPORT double GooglePlacePickerVersionNumber;
+
+//! Project version string for GooglePlacePicker.
+FOUNDATION_EXPORT const unsigned char GooglePlacePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GooglePlacePicker/PublicHeader.h>
+
+
